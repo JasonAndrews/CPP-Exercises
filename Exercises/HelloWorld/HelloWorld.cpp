6e62adaa20b160d39e7a3a25053abce8d3d8d211
jasonandrews271192@gmail.com <jasonandrews271192@gmail.com>
2015-08-24 17:42:27 +0100

    HelloWorld.cpp initial commit.

---
+++ Exercises/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: 24 Aug 2015
+ *      Author: Jason Andrews
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Starting first application!" << endl;
+}
+
+
+
