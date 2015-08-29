45da7e1efeb6105e2ec9e94a7b7222e8df1c9194
jasonandrews271192@gmail.com <jasonandrews271192@gmail.com>
2015-08-29 17:20:18 +0100

    ChildClasses initial commit. A simple program to demonstrate parent and child/derived/sub classes.

---
+++ Exercises/ChildClasses/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 29 Aug 2015
+ *      Author: Jason
+ */
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Application is running." << std::endl;
+
+}
+
+
